dd765aa250ce7e322fd980432b7b594ce3b45683
ItsJustChewbacca <itsjustchewbacca@gmail.com>
2018-01-16 18:07:06 +0000

    added in game logic and game class

---
+++ math_game.rb
@@ -0,0 +1,81 @@
+require 'colorize'
+require_relative 'player'
+require_relative 'question'
+
+
+
+#REPL + game logic
+@repl_bool = true
+#0 for p1 1 for p2
+@turn = 0
+
+puts "Welcome to ruby_math_game (OOP edition)"
+puts
+
+print 'Player 1 name: '
+@username = gets.chomp
+p1 = Player.new(@username)
+p1.name = p1.name.colorize(:cyan)
+print 'Player 2 name: '
+@username = gets.chomp
+p2 = Player.new(@username)
+p2.name = p2.name.colorize(:green)
+
+puts
+puts "Alright then, let us begin."
+puts
+
+while @repl_bool
+
+  question = Question.new
+
+  case @turn
+  when 0
+    print "#{p1.name}: "
+  else
+    print "#{p2.name}: "
+  end
+
+  print question.to_string
+  @user_input = gets.chomp.to_i
+
+  if @turn == 0 && question.is_correct?(@user_input)
+    puts "CORRECT ANSWER!".colorize(:light_blue)
+    p1.won
+    @turn += 1
+  elsif @turn == 0 && !question.is_correct?(@user_input)
+    puts "WRONG ANSWER!".colorize(:red)
+    p1.lost
+    @turn += 1
+  elsif @turn == 1 && question.is_correct?(@user_input)
+    puts "CORRECT ANSWER!".colorize(:light_blue)
+    p2.won
+    @turn -= 1
+  elsif @turn == 1 && !question.is_correct?(@user_input)
+    puts "WRONG ANSWER!".colorize(:red)
+    p2.lost
+    @turn -= 1
+  end
+
+  puts
+  puts "The score so far:
+        #{p1.name}: #{p1.correct_answers} CORRECT ANSWERS #{p1.score} REMAINING LIVES
+        #{p2.name}: #{p2.correct_answers} CORRECT ANSWERS #{p2.score} REMAINING LIVES"
+  puts
+
+  if p1.score < 1 || p2.score <1
+    puts "Game OVER."
+    if p1.score < 1
+      puts "The winner is #{p2.name}"
+    elsif p2.score < 1
+      puts "The winner is #{p1.name}"
+    end
+
+    puts "The final score was:
+        #{p1.name}: #{p1.correct_answers} CORRECT ANSWERS  #{p1.score} REMAINING LIVES
+        #{p2.name}: #{p2.correct_answers} CORRECT ANSWERS  #{p2.score} REMAINING LIVES"
+
+    @repl_bool = false
+  end
+
+end
